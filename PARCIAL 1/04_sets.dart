af8cceeac6ba2102a71c34a7e6550f4f46ed2aef
Yashiuwu <yashi.xuwux@gmail.com>
2023-01-11 11:32:24 -0600

    Nuevos proyectos agregados

--- PARCIAL 1/04_sets.dart
+++
@@ -1,6 +0,0 @@
-void main(List<String> args) {
-  //ejemplo de sets
-  Set<String> Estudiantes = {"Leonardo", "Evelyn", "Adrian", "Roberto", "LEONARDO"};
-
-  print(Estudiantes);
-}
---
+++ PARCIAL 1/Ejemplos/04_sets.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  //ejemplo de sets
+  Set<String> Estudiantes = {"Leonardo", "Evelyn", "Adrian", "Roberto", "Leonardo"};
+
+  print(Estudiantes);
+}
